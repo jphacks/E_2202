f811a3ad80150dfcf13d626a8836037936b15460
umahara555 <k228580@ie.u-ryukyu.ac.jp>
2022-10-17 02:05:46 +0900

    [WIP] Add components to search page

--- web/pages/search.tsx
+++ web/pages/search.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 
 export default function Search() {
   return (
@@ -18,6 +21,32 @@ export default function Search() {
         <Typography variant="h4" component="h1" gutterBottom>
           Search
         </Typography>
+
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          fullWidth
+          // value={age}
+          label="Age"
+          // onChange={handleChange}
+        >
+          <MenuItem value={1}>Python</MenuItem>
+          <MenuItem value={2}>JavaScript</MenuItem>
+          <MenuItem value={3}>Ruby</MenuItem>
+        </Select>
+
+        <p>
+
+        </p>
+
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          fullWidth
+          multiline
+          rows={10}
+          defaultValue="Default Value"
+        />
       </Box>
     </Container>
   );
