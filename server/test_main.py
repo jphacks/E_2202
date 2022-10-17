59e447f98829b9b89d82df5dc8a980755e3fa54f
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2022-10-17 23:41:55 +0900

    add parse_error_text api test

---
+++ server/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_parse_error_text():
+    response = client.post(
+        "error_parse",
+        json={"error_text": "node:internal/modules/cjs/loader:988\n  throw err;\n  ^\n\nError: Cannot find module '/usr/src/app/web/yarn.js'\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:985:15)\n    at Function.Module._load (node:internal/modules/cjs/loader:833:27)\n    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)\n    at node:internal/main/run_main_module:22:47 {\n  code: 'MODULE_NOT_FOUND',\n  requireStack: []\n}"}
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'result': ["Error: Cannot find module '/usr/src/app/web/yarn.js'", "  code: 'MODULE_NOT_FOUND',"]
+    }
