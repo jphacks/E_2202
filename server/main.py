7ce7acce726819748b2bd868fd9bb96e3e8a85ff
kagari <e175766@ie.u-ryukyu.ac.jp>
2022-10-17 17:02:11 +0900

    corsエラーが起こらないように全てallowするようにした

--- server/main.py
+++ server/main.py
@@ -5,9 +5,18 @@ from fastapi import (
 from pydantic import (
     BaseModel,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 error_name_pattern = re.compile(".*[e|E]rror.+")
 not_found_pattern = re.compile(".*N(ot|OT)[ |_]?F(ound|OUND).+")
 
