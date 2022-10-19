728084c5ff02b93d5de763dfff40ebd3a0480d88
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2022-10-19 18:55:45 +0900

    fixed main and test_main

--- server/test_main.py
+++ server/test_main.py
@@ -8,7 +8,13 @@ client = TestClient(app)
 def test_parse_error_text_passed():
     response = client.post(
         "error_parse",
-        json={"error_text": "node:internal/modules/cjs/loader:988\n  throw err;\n  ^\n\nError: Cannot find module '/usr/src/app/web/yarn.js'\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:985:15)\n    at Function.Module._load (node:internal/modules/cjs/loader:833:27)\n    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)\n    at node:internal/main/run_main_module:22:47 {\n  code: 'MODULE_NOT_FOUND',\n  requireStack: []\n}"}
+        json={"error_text": "node:internal/modules/cjs/loader:988\n  \
+            throw err;\n  ^\n\nError: Cannot find module '/usr/src/app/web/yarn.js'\n    \
+                at Function.Module._resolveFilename (node:internal/modules/cjs/loader:985:15)\n    \
+                    at Function.Module._load (node:internal/modules/cjs/loader:833:27)\n    \
+                        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)\n    \
+                            at node:internal/main/run_main_module:22:47 {\n  code: 'MODULE_NOT_FOUND',\n  \
+                                requireStack: []\n}"}
     )
     assert response.status_code == 200
     assert response.json() == {
