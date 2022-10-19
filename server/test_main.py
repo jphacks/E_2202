9c685946810f0197e495d6bd8ea67f66d8c3d8de
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2022-10-19 21:40:26 +0900

    レビュー修正

--- server/test_main.py
+++ server/test_main.py
@@ -10,11 +10,11 @@ def test_parse_error_text_passed() -> None:
         "error_parse",
         json={"error_text": "node:internal/modules/cjs/loader:988\n  \
             throw err;\n  ^\n\nError: Cannot find module '/usr/src/app/web/yarn.js'\n    \
-                at Function.Module._resolveFilename (node:internal/modules/cjs/loader:985:15)\n    \
-                    at Function.Module._load (node:internal/modules/cjs/loader:833:27)\n    \
-                        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)\n    \
-                            at node:internal/main/run_main_module:22:47 {\n  code: 'MODULE_NOT_FOUND',\n  \
-                                requireStack: []\n}"}
+            at Function.Module._resolveFilename (node:internal/modules/cjs/loader:985:15)\n    \
+            at Function.Module._load (node:internal/modules/cjs/loader:833:27)\n    \
+            at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)\n    \
+            at node:internal/main/run_main_module:22:47 {\n  code: 'MODULE_NOT_FOUND',\n  \
+            requireStack: []\n}"}
     )
     assert response.status_code == 200
     assert response.json() == {
