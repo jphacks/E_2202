ff7a838983b8d6f98dbe1764adb0c7382c8c32e5
soto <soto@cr.ie.u-ryukyu.ac.jp>
2022-10-21 20:50:18 +0900

    #45 Fix APIから帰ってきた結果で検索を行うように修正

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -46,7 +46,7 @@ export default function Search() {
       .then((data) => {
         console.log(data);
         setQueryErrorContents(data.result as []);
-        return router.push(`https://google.com/search?q=${os}+${language}+${error}+-site:sejuku.net&lr=lang_ja`)
+        return router.push(`https://google.com/search?q=${data.result as []}+-site:sejuku.net&lr=lang_ja`)
       });
   };
 
